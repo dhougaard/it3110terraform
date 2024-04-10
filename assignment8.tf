81f29d9259f61ce166f39143012dc5687387ec0e
Dallin Hougaard <79608579+dhougaard@users.noreply.github.com>
2024-04-10 06:59:26 +0000

    assignment8

--- assignment8.tf
+++ assignment8.tf
@@ -86,7 +86,7 @@ resource "aws_key_pair" "tf-key" {
 }
 
 # Create EC2 instances
-resource "aws_instance" "dev" {
+resource "aws_instance" "instance" {
   ami                    = "ami-080e1f13689e07408"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.tf-subnet.id
@@ -95,7 +95,7 @@ resource "aws_instance" "dev" {
   key_name               = aws_key_pair.tf-key.key_name
 
   tags = {
-    Name = "dev"
+    Name = "Instance"
   }
 
   user_data = <<-EOF
@@ -106,57 +106,21 @@ resource "aws_instance" "dev" {
          EOF
 }
 
-resource "aws_instance" "test" {
-  ami                    = "ami-080e1f13689e07408"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.tf-subnet.id
-  vpc_security_group_ids        = [aws_security_group.tf-sg.id]
-  associate_public_ip_address = true
-  key_name               = aws_key_pair.tf-key.key_name
-
+resource "aws_ebs_volume" "extra_storage" {
+  availability_zone = aws_instance.instance.availability_zone
+  size              = 5
   tags = {
-    Name = "test"
+    Name = "week10storage"
   }
-
-  user_data = <<-EOF
-         #!/bin/bash
-         wget http://computing.utahtech.edu/it/3110/notes/2021/terraform/install.sh -O /tmp/install.sh
-         chmod +x /tmp/install.sh
-         source /tmp/install.sh
-         EOF
 }
 
-resource "aws_instance" "prod" {
-  ami                    = "ami-080e1f13689e07408"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.tf-subnet.id
-  vpc_security_group_ids        = [aws_security_group.tf-sg.id]
-  associate_public_ip_address = true
-  key_name               = aws_key_pair.tf-key.key_name
-
-  tags = {
-    Name = "prod"
-  }
-
-  user_data = <<-EOF
-         #!/bin/bash
-         wget http://computing.utahtech.edu/it/3110/notes/2021/terraform/install.sh -O /tmp/install.sh
-         chmod +x /tmp/install.sh
-         source /tmp/install.sh
-         EOF
+resource "aws_volume_attachment" "ebs_attach" {
+  device_name = "/dev/sdv"
+  volume_id   = aws_ebs_volume.extra_storage.id
+  instance_id = aws_instance.instance.id
 }
 
-output "ip_of_dev" {
-  value		= aws_instance.dev.public_ip
-  description	= "Public IP of dev server"
-}
-
-output "ip_of_test" {
-  value		= aws_instance.test.public_ip
-  description	= "Public IP of test server"
-}
-
-output "ip_of_prod" {
-  value		= aws_instance.prod.public_ip
-  description	= "Public IP of prod server"
+output "ip_of_instance" {
+  value         = aws_instance.instance.public_ip
+  description   = "Public IP of github created instance"
 }
