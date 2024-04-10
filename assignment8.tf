dac681a737cfeea5b2306fad69b3ddb253a58810
dhougaard <d00415511@dmail.dixie.edu>
2024-04-10 16:14:43 +0000

    open

--- assignment8.tf
+++ assignment8.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "tf-sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
